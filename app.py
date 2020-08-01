91311b7e5986d12cf9cec95b119d5c9ee9c1d290
Galdir Reges <galdir@gmail.com>
2020-08-01 18:37:11 -0300

    css

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import plotly.express as px
 
 # external CSS stylesheets
 external_stylesheets = [
-    'https://raw.githubusercontent.com/galdir/covid19python/master/bWLwgP.css',
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
     {
         'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
         'rel': 'stylesheet',
